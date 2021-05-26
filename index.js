a30e95f6f8a528ccef8c1fccab1f1352b7760027
julianxlozano <julianxlozano@gmail.com>
2021-05-26 13:50:58 -0500

    commit tree 9011aecc0726398e7e50bbb22392c1afa6f346d4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function superbowlWin(array){
+   let win = array.find(function(record){return record.result === "W" })
+   if (!!win === false){
+        return 
+   }else{
+    return win.year}
 
+};
